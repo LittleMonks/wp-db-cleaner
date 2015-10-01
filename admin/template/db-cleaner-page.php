13f5f0899bd6b02ae03a55bf89d5ed6342720c0e
Utkarsh Patel <iamutkarsh@live.com>
2015-10-01 10:36:09 +0530

    Tab added

--- admin/template/db-cleaner-page.php
+++ admin/template/db-cleaner-page.php
@@ -14,4 +14,24 @@
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
-<h1>Hello World!</h1>
+<div class='wrap'>
+
+	<h2 class="nav-tab-wrapper">
+		<a href="<?php echo esc_url( admin_url( 'tools.php?page=db-clean' ) ); ?>"
+		   class="nav-tab <?php echo ( ! isset( $_REQUEST['subpage'] ) ) ? 'nav-tab-active' : ''; ?>">Find/Remove Orphan
+			Data</a>
+		<a href="<?php echo esc_url( admin_url( 'tools.php?page=db-clean&subpage=2' ) ); ?>"
+		   class="nav-tab <?php echo isset( $_REQUEST['subpage'] ) ? 'nav-tab-active' : ''; ?>">Find/Remove Duplicate
+			Data</a>
+	</h2>
+
+	<?php
+	if ( ! isset( $_REQUEST['subpage'] ) ) { ?>
+		<h1>Orhpan tab</h1>
+		<?php
+	} else { ?>
+		<h1>Duplicate tab</h1>
+		<?php
+	}
+	?>
+</div>
